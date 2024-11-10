e4dfc072f808b8f8543a014ec4a94b5758806a06
mediumlysalted <gabea38135@yahoo.com>
2024-11-10 15:34:10 -0600

    Fixed some errors, refactored top bar element, started on multiplayer

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ class Game(tk.Tk):
 
         # Populates pages with the different page classes after loading them
         self.pages = {}
-        for F in (MainMenu, SPMenu):
+        for F in (MainMenu, SPMenu, MPMenu):
             frame = F(game_frame, self)
             self.pages[F] = frame 
             frame.place(x=0, y=0, width=1024, height=786) 
@@ -26,14 +26,11 @@ class Game(tk.Tk):
 
     # Displays the current page
     def show_page(self, page_class):
-        print(f"\nDisplaying {page_class.__name__} page")
         page = self.pages[page_class]
         page.tkraise()
     
     def quit(self):
-        print("\nExiting Game...")
         self.destroy()
-        print("Done!")
 
 
 class MainMenu(tk.Frame):
@@ -83,6 +80,7 @@ class MainMenu(tk.Frame):
             activebackground=COLORS['red btn'].dark(),
             borderwidth=0,
             compound="center",
+            command=lambda : controller.show_page(MPMenu)
         )
         multiplayer_btn.place(
             relx=.515,
@@ -134,49 +132,9 @@ class SPMenu(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         self.controller = controller
-
         self.configure(bg=COLORS['background'])
-
-        # TOP BAR ELEMENTS
-        self.top_bar = tk.Frame(
-            self,
-            bg=COLORS['background'].dark()
-        )
-        self.top_bar.place(relx=0, rely=0, relwidth=1, relheight=.1)
-
-        title_text = tk.Label(
-            self.top_bar,
-            text="Single Player",
-            font=(GAME_FONT, 32),
-            justify='center',
-            fg=COLORS['yellow txt'],
-            background=COLORS["background"].dark(),
-        )
-        title_text.place(relx=.5, rely=.5, anchor='center')
-
-        self.back_icon = tk.PhotoImage(file='assets/left.png')
-        self.back_btn = tk.Button(
-            self.top_bar,
-            image=self.back_icon,
-            background=COLORS['background'].dark(),
-            activebackground=COLORS['background'].dark(),
-            borderwidth=0,
-            compound="center",
-            command=self.back
-        )
-        self.back_btn.place(relx=.025, rely=0.5, anchor='w')
-
-        self.quit_icon = tk.PhotoImage(file='assets/quit.png')
-        self.quit_btn = tk.Button(
-            self.top_bar,
-            image=self.quit_icon,
-            background=COLORS['background'].dark(),
-            activebackground=COLORS['background'].dark(),
-            borderwidth=0,
-            compound="center",
-            command=self.controller.quit
-        )
-        self.quit_btn.place(relx=.975, rely=0.5, anchor='e')
+        self.top_bar = TopBar(self, "Single Player").place(relx=0, rely=0, relwidth=1, relheight=.1)
+        self.game = None
 
         # SIDE BAR ELEMENTS
         self.side_bar = tk.Frame(
@@ -214,7 +172,7 @@ class SPMenu(tk.Frame):
             self.side_bar,
             bg=COLORS['background'],
         )
-        self.info_box.place(relx=.5, rely=.025, relwidth=.65, relheight=.2, anchor='n')
+        self.info_box.place(relx=.5, rely=.025, relwidth=.65, relheight=.15, anchor='n')
 
         self.timer = tk.Label(
             self.info_box,
@@ -224,7 +182,7 @@ class SPMenu(tk.Frame):
             fg=COLORS['yellow txt'],
             background=COLORS["background"],
         )
-        self.timer.place(relx=.05, rely=.3, anchor='w')
+        self.timer.place(relx=.05, rely=.25, anchor='w')
 
         self.bomb_icon = tk.PhotoImage(file='assets/bomb.png')
         self.bomb_label = tk.Label(
@@ -234,7 +192,7 @@ class SPMenu(tk.Frame):
             borderwidth=0,
             compound="center",
         )
-        self.bomb_label.place(relx=.25, rely=.7, anchor='w')
+        self.bomb_label.place(relx=.25, rely=.75, anchor='w')
         self.bomb_count = tk.Label(
             self.info_box,
             text='0',
@@ -243,7 +201,7 @@ class SPMenu(tk.Frame):
             fg=COLORS['yellow txt'],
             background=COLORS["background"],
         )
-        self.bomb_count.place(relx=.75, rely=.7, anchor='e')
+        self.bomb_count.place(relx=.75, rely=.75, anchor='e')
 
         # GAME ELEMENTS
         self.minefield_frame = tk.Frame(
@@ -265,8 +223,9 @@ class SPMenu(tk.Frame):
     def stop_game(self):
         self.start_btn.configure(image=self.start_icon)
         self.start_btn.configure(command=self.start_game)
-        self.game.game_over = True
+        if self.game: self.game.game_over = True
         self.timer.configure(text=f'00:00.0')
+        self.bomb_count.configure(text='0')
         for widget in self.minefield_frame.winfo_children():
             widget.destroy()
 
@@ -279,7 +238,7 @@ class SPMenu(tk.Frame):
 
     def reset_game(self):
         self.stop_game()
-        self.start_game()
+        self.controller.after(100, self.start_game)
 
     def back(self):
         self.stop_game()
@@ -290,10 +249,60 @@ class MPMenu(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        self.controller = controller
+        self.configure(bg=COLORS['background'])
+        TopBar(self, "Multiplayer").place(relx=0, rely=0, relwidth=1, relheight=.1)
+        self.game = None
+
+    def back(self):
+        self.controller.show_page(MainMenu)
+
 
 class H2PMenu(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+
+class TopBar(tk.Frame):
+    def __init__(self, parent, menu_name):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.configure(bg=COLORS['background'].dark())
+
+        # TOP BAR ELEMENTS
+        title_text = tk.Label(
+            self,
+            text=menu_name,
+            font=(GAME_FONT, 32),
+            justify='center',
+            fg=COLORS['yellow txt'],
+            background=COLORS["background"].dark(),
+        )
+        title_text.place(relx=.5, rely=.5, anchor='center')
+
+        self.back_icon = tk.PhotoImage(file='assets/left.png')
+        self.back_btn = tk.Button(
+            self,
+            image=self.back_icon,
+            background=COLORS['background'].dark(),
+            activebackground=COLORS['background'].dark(),
+            borderwidth=0,
+            compound="center",
+            command=self.parent.back
+        )
+        self.back_btn.place(relx=.025, rely=0.5, anchor='w')
+
+        self.quit_icon = tk.PhotoImage(file='assets/quit.png')
+        self.quit_btn = tk.Button(
+            self,
+            image=self.quit_icon,
+            background=COLORS['background'].dark(),
+            activebackground=COLORS['background'].dark(),
+            borderwidth=0,
+            compound="center",
+            command=self.parent.controller.quit
+        )
+        self.quit_btn.place(relx=.975, rely=0.5, anchor='e')
 
 
 def load_font(font_path):
